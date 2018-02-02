f62d8648ae8b71fb4567fb0bcf5f67b7c27e2790
Malik Baba Aïssa <malik@MacBook-Pro-de-Malik.local>
2018-02-02 18:28:53 +0100

    first version of the bubbles

---
+++ src/components/Bubble.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg :width="size" :height="size" :viewBox="`0 0 ${size} ${size}`" class="bubble" xmlns="http://www.w3.org/2000/svg" :style="`left:${posX}%`">
+    <defs>
+      <linearGradient id="a" x1="50%" x2="50%" y1="100%" y2="0%">
+        <stop offset="0%" stop-color="#D6D2FD"/>
+        <stop offset="100%" stop-color="#FFF"/>
+      </linearGradient>
+    </defs>
+    <circle
+      :cx="`${size / 2}`"
+      :cy="`${size / 2}`"
+      :r="`${size / 2}`"
+      fill="url(#a)"
+      fill-rule="evenodd"
+    />
+  </svg>
+</template>
+
+<script>
+// import anime from 'animejs';
+
+export default {
+  name: 'Bubble',
+  props: ['size', 'posX'],
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+
+// window.onload = () => {
+//   anime({
+//     targets: '#moving-bubbles .bubble',
+//     translateY: [-200, 2000],
+//     delay: 500,
+//     direction: 'alternate',
+//     loop: true,
+//     duration: (el, i) => 1000 + (i * 1000),
+//   });
+// };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .small-image {
+    width: 10%;
+  }
+
+  .bubble {
+    display: block;
+    position: absolute;
+    bottom: 0;
+    opacity: 0;
+    transform-origin: bottom center;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="hero is-fullheight is-primary">
+  <section class="hero is-fullheight">
 
     <div class="hero-body">
       <div class="container has-text-centered">
-        <h3 class="title is-2">
+        <h3 class="title is-2" id="my-name">
           <a href="https://www.linkedin.com/in/malikba/">
             Malik <strong>Baba Aïssa</strong>
           </a>
@@ -18,24 +18,73 @@
           <ul>
             <li class="small-image">
               <a href="https://bulma.io">
-                <img src="../assets/img/made-with-bulma--semiwhite.png" alt="bulma logo">
+                <img src="../assets/img/made-with-bulma--semiblack.png" alt="bulma logo">
               </a>
             </li>
           </ul>
         </div>
       </div>
     </div>
+
+    <div v-for="bubble in bubbles" :key="bubble.posX" id="moving-bubbles">
+      <Bubble :size="bubble.size" :posX="bubble.posX" />
+    </div>
   </section>
+
+
 </template>
 
 <script>
+import anime from 'animejs';
+import Bubble from './Bubble';
+
+const bubbles = (() => {
+  const maxSize = 100;
+  const minSize = 20;
+  const res = [];
+
+  for (let i = 0; i < 100; i += 1) {
+    const size = (Math.random() * (maxSize - minSize)) + minSize;
+    const posX = Math.random() * 95;
+    res.push({ size, posX });
+  }
+
+  return res;
+})();
+
 export default {
   name: 'Home',
+  components: {
+    Bubble,
+  },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      bubbles,
     };
   },
+};
+
+window.onload = () => {
+  anime(
+    // {
+    //   targets: '#my-name',
+    //   translateY: [50, 0],
+    //   opacity: [0, 1],
+    //   delay: 500,
+    //   duration: 4000,
+    //   direction: 'alternate',
+    //   loop: false,
+    // },
+    {
+      targets: '#moving-bubbles .bubble',
+      translateY: -500,
+      delay: 500,
+      opacity: 1,
+      direction: 'alternate',
+      loop: true,
+      duration: (el, i) => Math.random() * (i * 1000),
+    });
 };
 </script>
 
@@ -43,5 +92,15 @@ export default {
 <style scoped>
   .small-image {
     width: 10%;
+  }
+
+  .hero.is-fullheight:before {
+    content: '';
+    display: block;
+    width: calc(100% - 10px);
+    height: calc(100% - 10px);
+    position: absolute;
+    border: 1px solid #e8dfdf;
+    margin: 5px;
   }
 </style>
