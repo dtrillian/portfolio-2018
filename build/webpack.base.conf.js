39ceef2b10931d5b872deb3fa356727de5128942
Malik Baba Aïssa <malik@MacBook-Pro-de-Malik.local>
2018-02-02 14:42:50 +0100

    scss + first page

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,10 +32,11 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.scss'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      styles: path.resolve(__dirname, '../src/assets/scss') // relative to the location of the webpack config file!
     }
   },
   module: {
---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="hero is-fullheight is-primary">
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h3 class="title is-2">
+          <a href="https://www.linkedin.com/in/malikba/">
+            Malik <strong>Baba Aïssa</strong>
+          </a>
+        </h3>
+        <h4 class="subtitle is-4">Portfolio</h4>
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <div class="container">
+        <div class="tabs is-centered">
+          <ul>
+            <li class="small-image">
+              <a href="https://bulma.io">
+                <img src="../assets/img/made-with-bulma--semiwhite.png" alt="bulma logo">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .small-image {
+    width: 10%;
+  }
+</style>
